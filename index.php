ff94aaa9f852a3551ab9d155596b7ce2c70cb39b
AlessandroLembo <alessandrolembo2@gmail.com>
2023-02-17 16:24:01 +0100

    create a construct function in class Movie

--- index.php
+++ index.php
@@ -4,9 +4,30 @@ class Movie
     public $title;
     public $director;
     public $year;
-    public $vote;
     public $genre;
+    public $vote;
+
+
+    public function __construct($_title, $_director, $_year, $_genre, $_vote = Null)
+    {
+        $this->title = $_title;
+        $this->director = $_director;
+        $this->year = $_year;
+        $this->vote = $_vote;
+        $this->genre = $_genre;
+    }
 }
+
+$first_movie = new Movie('Pulp fiction', 'Quentin tarantino', 1994, 'gangster');
+
+$second_movie = new Movie('Arancia meccanica', 'Stanley Kubrik', 1971, 'drammatico');
+
+var_dump($first_movie);
+var_dump($second_movie);
+
+
+
+
 ?>
 
 <!DOCTYPE html>
